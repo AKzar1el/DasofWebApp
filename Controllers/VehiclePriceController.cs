310d444c418bc256ac77ce60700fe3d3df197b74
TOMI ŠEREGI <ts8021@student.uni-lj.si>
2024-02-04 13:50:53 +0100

    Add VehiclePriceController.

---
+++ Controllers/VehiclePriceController.cs
@@ -0,0 +1,16 @@
+﻿using DasofWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DasofWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehiclePriceController
+    {
+        [HttpPost]
+        public ActionResult<VehiclePrice> GetVehiclePrice(VehiclePrice vehiclePrice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
